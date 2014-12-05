2d985d08ce4a1afaf83406913f563933e11d92ec
witkowski01 <witkowski01@wp.pl>
2014-12-05 14:58:22 +0100

    Początek+test autokorelacji

---
+++ Tester_losowości/Tester_losowości/Model/Test.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester_losowości.Model
+{
+    class Test
+    {
+
+
+        public bool[] sample;
+
+        public Test(string s)
+        {
+            StringToBool(s);
+        }
+
+        public void StringToBool(string s)
+        {
+            sample = new bool[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                sample[i] = s[i] == '1' ? true : false;
+            }
+        }
+
+
+
+
+
+        #region Podstawowe testy statystyczne
+        #region Test autokorelacji
+
+        public double Test_Autokorelacji()
+        {
+            const int s = 5;
+            return (double) (2*liczba_bitowA(s) - sample.Length + s)/Math.Sqrt((double) (sample.Length - s));
+        }
+
+        public int liczba_bitowA(int s)
+        {
+            int wyjscie = 0;
+            for (int i = 0; i < sample.Length - s; i++)
+            {
+                wyjscie += sample[i] ^ sample[i + s] ? 1 : 0;
+            }
+            return wyjscie;
+        }
+
+        #endregion
+
+
+        #region Test par bitów
+
+        #endregion
+
+        #endregion
+
+        #region Testy statystyczne według FIPS 140-2
+
+        #region Test długiej serii
+
+        #endregion
+
+        #region Test pojedynczych bitów
+
+        #endregion
+
+        #endregion
+    }
+}
